d06953159379c31a671a1b4923fdf04ac8e441a8
lXSPandora <luizepauloxd@gmail.com>
2018-08-15 15:08:31 -0300

    ✨ Adds encrypt function

--- lib/elixirblog/account/user.ex
+++ lib/elixirblog/account/user.ex
@@ -2,14 +2,27 @@ defmodule Elixirblog.Account.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
-    field :email, :string
-    field :name, :string
-    field :password, :string
-    field :active, :boolean, default: true
+    field(:email, :string)
+    field(:name, :string)
+    field(:password, :string)
+    field(:active, :boolean, default: true)
 
     timestamps()
+  end
+
+  defp encrypt_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(
+          changeset,
+          :password,
+          Bcrypt.hash_pwd_salt(password)
+        )
+
+      _ ->
+        changeset
+    end
   end
 
   @doc false
@@ -17,5 +30,6 @@ defmodule Elixirblog.Account.User do
     user
     |> cast(attrs, [:name, :email, :password, :active])
     |> validate_required([:name, :email, :password, :active])
+    |> encrypt_password()
   end
 end
