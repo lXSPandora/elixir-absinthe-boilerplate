364b982529cde38aada502ce7169d0cb6f5037f6
lXSPandora <luizepauloxd@gmail.com>
2018-08-18 02:41:30 -0300

    ✨ adds resolvers for user authentication

--- lib/elixirblog_web/graphql/resolvers/UserResolvers.ex
+++ lib/elixirblog_web/graphql/resolvers/UserResolvers.ex
@@ -1,5 +1,7 @@
 defmodule Elixirblog.UserResolvers do
   alias Elixirblog.Account
+  alias Elixirblog.Repo
+  import Ecto.Query, warn: false
 
   def getUser(_parent, %{id: id}, _info) do
     case Account.get_user!(id) do
@@ -14,12 +16,42 @@ defmodule Elixirblog.UserResolvers do
     {:ok, Account.list_users()}
   end
 
+  def me(_args, %{context: %{current_user: current_user}}) do
+    {:ok, current_user}
+  end
+
+  def login(_parent, %{email: email, password: password}, _info) do
+    query = from u in "users",
+          where: u.email == ^email,
+          select: u.password
+    case Repo.one(query) do
+      nil ->
+        {:error, "Invalid email or password"}
+      hash ->
+        case Bcrypt.verify_pass(password, hash) do
+          false ->
+            {:error, "Invalid email or password"}
+          true ->
+            token = JsonWebToken.sign(%{email: email}, %{key: "gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C"})
+            {:ok, %{token: token}}
+        end
+    end
+  end
+
   def registerEmail(_parent, args, _info) do
-    case Account.create_user(args) do
-      {:ok, user} ->
-        {:ok, user}
-      _error ->
-        {:error, "could not create link"}
+    query = from u in "users",
+          where: u.email == ^args.email,
+          select: u.password
+    case Repo.one(query) do
+      nil ->
+        case Account.create_user(args) do
+          {:ok, user} ->
+            {:ok, user}
+          _error ->
+            {:error, "Something went wrong while we create the user"}
+        end
+      _user ->
+        {:error, "This email is already registered on the database"}
     end
   end
 end
